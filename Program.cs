39038d86d70dc6828074e998127dcd0fec1d88b7
Carlos Lopez <carlos@wecancodeit.org>
2021-12-26 15:07:17 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace template_csharp_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Standard code with a new console application
+            Console.WriteLine("Hello World!");
+
+            // Step 1: Enter your name in between the quotation marks
+            string name = "YOUR NAME HERE";
+
+            Console.WriteLine("Welcome to the Hello World application, " + name);
+
+            // Step 2: Research Console.WriteLine
+            // https://docs.microsoft.com/en-us/dotnet/api/system.console.writeline?view=net-6.0
+            // Step 3: Use 3 variations of Console.WriteLine
+
+            // --- START CODE HERE ---
+
+
+            // --- END CODE HERE ---
+
+            // This line is to pause the application without storing input
+            Console.Read();
+
+            // This line clears the console screen
+            Console.Clear();
+
+            // Step 4: Research Console.Read, Console.ReadLine
+            // https://docs.microsoft.com/en-us/dotnet/api/system.console.read?view=net-6.0
+            // https://docs.microsoft.com/en-us/dotnet/api/system.console.readline?view=net-6.0
+            // Step 5: Use Console.ReadLine to get user input, store it as a string
+
+            // --- START CODE HERE ---
+
+
+            // --- END CODE HERE ---
+
+            // Step 3: Set each data type to an appropriate non-zero number
+            int a = 0;
+            double b = 0;
+            float c = 0;
+            long d = 0;
+
+            Console.WriteLine("The value of the integer is now {0}, the double is now {1}, the float is now {2}, the long is now {3}", a, b, c, d);
+
+
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+﻿# We Can Code It
+
+## Introduction
+
+> Many programming tutorials start with a "Hello World" project. This is normally considered a working proof of concept for whatever programming language you are using.
+
+## Code Samples
+
+> You should read and practice using some of the tutorials/examples found on the [Microsoft Documentation - Console class](https://docs.microsoft.com/en-us/dotnet/api/system.console?view=net-6.0)
+<br /><br />
+>When you are using Console.WriteLine or Console.ReadLine what is it that you using? Look up programming methods for a hint.
+<br /><br />
+>Datatypes like int, double, float, and sometimes string are considered  simple types.
+<br /><br />
+>C# is a strongly typed language that means in coding the data type must always proceed the variable declaration. Read more about data types on [TutorialsTeacher.com/csharp/csharp-data-types](https://www.tutorialsteacher.com/csharp/csharp-data-types)
+
+## Installation
+
+> When considering if you need a variable, you must first ask, what type of data do I need to store this variable.
+
+>If a method returns something, think first about what type the method returns, then start your statement: 
+<br /><br />
+<code>data type | variable name | assignment operator | method being called</code>
+<br /><br />
+>Example:
+<br /><br />
+<code>string myName = Console.ReadLine()</code>
